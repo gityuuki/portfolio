c624046c595b5e03a707a01ef8d0dc9053c3efd4
EC2 Default User <ec2-user@ip-172-31-11-114.ap-northeast-1.compute.internal>
2023-09-12 11:01:53 +0000

    main

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root "public/homes#top"
-  namespace :public do
+  devise_for :users
+  root "homes#top"
     get 'homes/about'
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
