44db6e32bbfa3bc432d5b99bc96eee93e7431b59
EC2 Default User <ec2-user@ip-172-31-11-114.ap-northeast-1.compute.internal>
2023-09-21 11:02:48 +0000

    main

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'relationships/followers'
   get 'relationships/followings'
-  get 'users/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'recruitments/edit'
-  get 'recruitments/index'
-  get 'recruitments/show'
-  devise_for :users
+  resources :recruitments, only: [:new, :index, :show, :create, :edit, :destroy, :update]
+  resources :users, only: [:show, :edit, :update, :index]
   root "homes#top"
  get "home/about"=>"homes#about"
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
